28964215fcf7e4f01e1ae42cee9651ad72737bfe
Joao Antonio <joaoantant@gmail.com>
2022-11-03 15:48:00 +0000

    included old bayesian networks files

--- Multivariate Statistics/R scripts/exerc4_5.r
+++ Multivariate Statistics/R scripts/exerc4_5.r
@@ -6,3 +6,5 @@ print(colMeans(food[, -1]))
 
 print("The Standard Deviation of the foods")
 sapply(food[, -1], sd)
+
+print("Answer to question 2, we will use the correlation matrix since we will do a normalisation, because the variance of the data are very different.")
