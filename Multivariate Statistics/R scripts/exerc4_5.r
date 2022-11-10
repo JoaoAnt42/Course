d07e862ea52222aad851e7cfa94b7ca2d320bf5b
Joao Antonio <joaoantant@gmail.com>
2022-11-10 10:09:05 +0000

    Exerc 4_5

--- Multivariate Statistics/R scripts/exerc4_5.r
+++ Multivariate Statistics/R scripts/exerc4_5.r
@@ -4,7 +4,15 @@ View(food)
 print("The mean of the foods")
 print(colMeans(food[, -1]))
 
+
 print("The Standard Deviation of the foods")
 sapply(food[, -1], sd)
 
-print("Answer to question 2, we will use the correlation matrix since we will do a normalisation, because the variance of the data are very different.")
+print("Answer to question 2, we will use the correlation matrix since we will do a normalisation, because the variance of the data are very different. The var of the var is given by")
+print(var(sapply(food[, -1], var)))
+
+print("Normalize the data")
+# Normalize the data
+pca_food <- prcomp(food[, -1])
+
+print(summary(pca_food))
