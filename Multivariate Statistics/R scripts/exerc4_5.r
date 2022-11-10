92668c61ead550f5f822cd2ab9b3b5987239dd41
Joao Antonio <joaoantant@gmail.com>
2022-11-10 10:28:37 +0000

    exerc 4_5

--- Multivariate Statistics/R scripts/exerc4_5.r
+++ Multivariate Statistics/R scripts/exerc4_5.r
@@ -13,5 +13,11 @@ print(var(sapply(food[, -1], var)))
 print("Normalize the data")
 # Normalize the data
 pca_food <- prcomp(food[, -1], scale = TRUE)
+sum_food <- summary(pca_food)
+print(sum_food)
+print(sprintf("PC_1 = %s (product - mean(product))(sd(product))", pca_food[1]))
+print(sprintf("PC_2 = %s (product - mean(product))(sd(product))", pca_food[2]))
 
-print(summary(pca_food))
+print("Corrleation between our data and our original data")
+print(round(cor(food[, -1], pca_food$x), 2))
+# print(sprintf("PC_2 is equal to %s X_1 +  %s X_2", eigen(sc)$vectors[3], eigen(sc)$vectors[4]))
